4c9acc2837f4deace9f7514921eeee82b0279432
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-01-10 13:27:42 +0000

    log hello word

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 let express = require('express');
 let app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+// const listener = app.listen(process.env.PORT || 5000, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+
 
 
 
